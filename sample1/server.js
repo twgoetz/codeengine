52aa6d8d631223a59cb739fe8d07499b12ddc353
Thilo Goetz <tgoetz@de.ibm.com>
2021-06-21 09:53:47 +0200

    CORS enable sample

--- sample1/server.js
+++ sample1/server.js
@@ -1,10 +1,15 @@
-const http = require('http');
 
-http.createServer(function (request, response) {
-   target = process.env.TARGET ? process.env.TARGET : 'World' ;
-   msg = process.env.MSG ? process.env.MSG : 'Hello ' + target + '\n';
-   response.writeHead(200, {'Content-Type': 'text/plain'});
-   response.end(msg);
-}).listen(8080);
+const express = require('express');
+const cors = require('cors');
+const app = express();
+ 
+app.use(cors());
+ 
+app.get('/', (req, res) => {
+  res.json({msg: 'Hello CORS!'})
+})
 
-console.log('Server running at http://0.0.0.0:8080/');
+const port = 8080;
+app.listen(port, () => {
+  console.log(`CORS-enabled web server listening on port ${port}`);
+});
