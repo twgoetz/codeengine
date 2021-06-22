2f8a4b175606b7c91f575c2a9ccbb361b70d9450
Thilo Goetz <tgoetz@de.ibm.com>
2021-06-22 16:15:07 +0200

    Send back funky CE logo

--- sample1/server.js
+++ sample1/server.js
@@ -5,9 +5,21 @@ const app = express();
 
 app.use(cors());
 
+const bodyText = ```
+. ___  __  ____  ____
+./ __)/  \(    \(  __)
+( (__(  O )) D ( ) _)
+.\___)\__/(____/(____)
+.____  __ _   ___  __  __ _  ____
+(  __)(  ( \ / __)(  )(  ( \(  __)
+.) _) /    /( (_ \ )( /    / ) _)
+(____)\_)__) \___/(__)\_)__)(____)
+
+```;
+
 app.get('/', (req, res) => {
-   res.set('Content-Type', 'application/json');
-   res.json({ msg: 'Hello CORS!' })
+   res.set('Content-Type', 'text/plain');
+   res.send(bodyText);
 })
 
 const port = 8080;
