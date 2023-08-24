fc30dbd94dc93395af2b3504db6483a8c8c94f53
Thilo Goetz <tgoetz@de.ibm.com>
2023-08-24 19:25:25 +0200

    fix date computation

--- probes-test-app/index.js
+++ probes-test-app/index.js
@@ -19,7 +19,7 @@ const isReady = () => {
   if (ready === 0) {
     return true;
   }
-  const now = new Date().getDate();
+  const now = new Date().valueOf();
   return now > ready;
 };
 
@@ -51,9 +51,9 @@ app.post("/die", (req, res) => {
 app.post("/sleep", (req, res) => {
   const timeoutParam = req.params.timeout || '10';
   const timeout = Number.parseInt(timeoutParam) * 1_000;
-  const now = new Date().getDate();
+  const now = new Date().valueOf();
   ready = now + timeout;
-  res.status(200).send(`App will sleep for ${timeout} seconds until ${new Date(ready).toDateString()}`);
+  res.status(200).send(`App will sleep for ${timeoutParam} seconds until ${new Date(ready).toDateString()}`);
 });
 
 const port = 80;
